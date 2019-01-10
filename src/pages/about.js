dad467e018661d3230a9a411b4ddec1124e8f1b9
Bryan Smith <bryan.gsmith33@gmail.com>
2019-01-10 16:37:23 -0700

    updated about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const AboutHeroImg = styled.img`
   height: 500px;
   float: left;
   margin: 0 1em 1em 0;
-  @media (max-width: 800px){
+  @media (max-width: 800px) {
     width: 100%;
     height: 100%;
   }
@@ -41,10 +41,23 @@ const About = () => (
       <IntroText>
         Welcome to Wren lane. This is where we share our experiences in home
         renovation, fashion, health and our overall life. Collectively we are
-        the Smith's and for as plain as our last name may seem, we are an
-        eclectic group that creates our family who loves to live life to the
-        fullest.{' '}
+        the Smith's and for as plain as our last name may be, we are an eclectic
+        group that creates our family who loves to live life to the fullest.
       </IntroText>
+      <PersonTitle>Bryan</PersonTitle>
+      <AboutText>
+        Bryan is the main energy of our family. He is constantly making us
+        laugh, and keeping us all happy. Let's just say it now, he is the fun
+        parent! If you ever have the chance to hear Bryan tell a story, consider
+        yourself lucky. His attention to detail is incredible and you will feel
+        like you were actually there.
+      </AboutText>
+      <AboutText>
+        Bryan has a career in Web Development and built this entire site you are
+        currently on. He is very creative and has an eye for design. Hence why
+        we occasionally let him pick out things for our house. When Bryan isn't
+        building websites you can find him manicuring the lawn.
+      </AboutText>
       <PersonTitle>Lindsey</PersonTitle>
       <AboutText>
         Lindsey is truly the head of the Smith family, although Bryan may beg to
@@ -59,17 +72,17 @@ const About = () => (
         Fitness is one thing that drives Lindsey and she can often be found
         exercising and sharing her health journey with others. Her desire for
         improvement can be found in everything she does from renovating our
-        guest bathroom to creating a delicous mac n cheese recipe that is also
+        guest bathroom to creating mac n cheese that is both delicious and
         healthy.
       </AboutText>
       <PersonTitle>LitteLoveOne (LLO)</PersonTitle>
       <AboutText>
-        LittleLoveOne is our oldest daughter. She is the perfect mix between
-        Bryan and Lindsey. She is as goofy as can be and her belly laugh can be
-        heard throughout the entire house. She is also extremely caring,
-        sometimes so much so that LittleLoveTwo thinks it borders on smothering.
-        You can often find her wrestling the dogs and asking how she can help
-        her parents, even if it means getting her hands a little dirty.
+        LittleLoveOne is our oldest daughter. She is the perfect mix between the
+        two of us. She is as goofy as can be and her belly laugh can be heard
+        throughout the entire house. She is also extremely caring, sometimes so
+        much so that LittleLoveTwo thinks it borders on smothering. You can
+        often find her wrestling the dogs and asking how she can help her
+        parents, even if it means getting her hands a little dirty.
       </AboutText>
       <PersonTitle>LitteLoveTwo (LLT)</PersonTitle>
       <AboutText>
@@ -82,9 +95,9 @@ const About = () => (
       <AboutText>
         Sperry is our bigger black and white dog. He is a Australian
         Shepherd/Golden Retriever mix. The best way to describe Sperry is that
-        he doesn't know he is a dog. It's tough to describe but he is as odd a
-        dog that you will find. He shares a lot of the same personalites as the
-        other Smith's; kindness, goofiness and a love of adventure.
+        he doesn't know he is a dog. He is tough to describe but the best way to
+        is probably odd. He shares a lot of the same personalites as the other
+        Smith's; kindness, goofiness and a love of adventure.
       </AboutText>
       <PersonTitle>Finn</PersonTitle>
       <AboutText>
